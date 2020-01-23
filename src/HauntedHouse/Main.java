e81e1e97b8ba7a635dd98d4a757afbc02490b563
test@test.com <test@test.com>
2020-01-23 18:53:51 +0000

    test

--- src/HauntedHouse/Main.java
+++ src/HauntedHouse/Main.java
@@ -1,7 +1,9 @@
 package HauntedHouse;
 
+import ed.adt.OrderedListADT;
 import ed.exceptions.ElementNotFoundException;
 import ed.exceptions.EmptyCollectionException;
+import ed.util.ArrayOrderedList;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,9 +19,18 @@ public class Main {
             Map<Room> map = new Map<>();
             Room room1 = new Room("hall", (short) 1);
             Room room2 = new Room("sala de estar", (short) -1);
-            Iterator<Room> iter = map.iteratorShortestPath(room1, room2);
+            Iterator<Room> iter = map.iteratorBFS(room1);
+            
+            OrderedListADT<Integer> list = new ArrayOrderedList<>();
+            list.add(3);
+            list.add(4);
+            list.add(0);
+            list.add(25);
+            list.add(-4);
+            list.add(1);
+            System.out.println(list.toString());
             while (iter.hasNext()) {
-                System.out.println(iter.next().toString());
+                //System.out.println(iter.next().toString());
             }
         } catch (EmptyCollectionException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
